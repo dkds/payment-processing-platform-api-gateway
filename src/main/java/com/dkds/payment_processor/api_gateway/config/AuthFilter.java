789c7f9e2379afb9bf862f9eae8dfefd115e763a
dkds <dkds@users.noreply.github.com>
2025-02-25 21:06:38 +0530

    Refactor gateway to avoid hardcoded config

--- src/main/java/com/dkds/payment_processor/api_gateway/config/AuthFilter.java
+++ src/main/java/com/dkds/payment_processor/api_gateway/config/AuthFilter.java
@@ -2,22 +2,27 @@ package com.dkds.payment_processor.api_gateway.config;
 
 import com.dkds.payment_processor.api_gateway.util.JwtUtil;
 import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Configuration
-public class AuthFilter implements GatewayFilter {
+@Component
+@NoArgsConstructor
+public class AuthFilter implements GatewayFilterFactory<AuthFilter.Config> {
 
     @Autowired
     private JwtUtil jwtUtil;
 
-    @Override
+    //    @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String token = resolveToken(exchange);
         JWTClaimsSet jwtClaimsSet = jwtUtil.validateToken(token);
@@ -36,5 +41,31 @@ public class AuthFilter implements GatewayFilter {
             return bearerToken.substring(7);
         }
         return null;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            System.out.println("gateway filter name " + config.getName());
+            String token = resolveToken(exchange);
+            JWTClaimsSet jwtClaimsSet = jwtUtil.validateToken(token);
+            return chain.filter(exchange);
+        };
+    }
+
+    @Override
+    public Config newConfig() {
+        return new Config("AuthFilter");
+    }
+
+    @Override
+    public Class<Config> getConfigClass() {
+        return AuthFilter.Config.class;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Config {
+        private String name;
     }
 }
